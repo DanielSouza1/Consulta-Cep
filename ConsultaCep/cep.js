6c9f1cfd38da379f6963d2bf6b650b7d7fdf872c
DanielSouza1 <daniel.sf.ds11@gmail.com>
2020-09-17 15:45:36 -0300

    7

--- ConsultaCep/cep.js
+++ ConsultaCep/cep.js
@@ -1,15 +1,12 @@
-var titulo = document.getElementById('titu')
 var logo = document.getElementById('logo')
 var imagem = document.createElement('img')
 imagem.setAttribute('src', 'foto')
 imagem.setAttribute('src', '/gps.png')
 imagem.style.height = '70px'
 logo.appendChild(imagem)
-titulo.style.color = '#FFFF00'
 function Consultar(){
-    let cep1 = document.getElementById('Cep');  
-    cep1.focus() 
-    let cep2 = String(cep1.value);
+    let cep1 = document.getElementById('Cep');      
+    let cep2 = String(cep1.value);    
     if(cep2.length < 8){
         window.alert('ERRO! Cep não encontrado')
     }else{
@@ -19,15 +16,13 @@ function Consultar(){
         request.onload = function(){             
             let endereço = JSON.parse(request.responseText)
             if(endereço.erro == true){
-                window.alert('Cep Invalido!!!')
-                
+                window.alert('Cep Invalido!!!')                
             }else{
                 let logradouro = document.getElementById('logradouro').value = endereço.logradouro
+                
                 let bairro = document.getElementById('bairro').value = endereço.bairro
                 let local = document.getElementById('localidade').value = endereço.localidade
-            }          
-            
-            
+            }         
         }
         request.send();    
 
